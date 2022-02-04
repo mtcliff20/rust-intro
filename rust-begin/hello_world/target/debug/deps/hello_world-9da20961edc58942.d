c63b96f5cf0b97a9bf2d1522457f9ee04e3e6d62
Matt <mtclifford12@gmail.com>
2022-02-03 22:38:03 -0600

    update

---
+++ rust-begin/hello_world/target/debug/deps/hello_world-9da20961edc58942.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/Matt/Desktop/rust/rust-begin/hello_world/target/debug/deps/hello_world-9da20961edc58942: src/main.rs
+
+/mnt/c/Users/Matt/Desktop/rust/rust-begin/hello_world/target/debug/deps/hello_world-9da20961edc58942.d: src/main.rs
+
+src/main.rs:
---
+++ rust-begin/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/Matt/Desktop/rust/rust-begin/hello_world/target/debug/hello_world: /mnt/c/Users/Matt/Desktop/rust/rust-begin/hello_world/src/main.rs
