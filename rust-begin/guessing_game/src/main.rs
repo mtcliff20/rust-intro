19566ff68bbe2c85c8f051944c5390b7a5b67913
Matt <mtclifford12@gmail.com>
2022-02-03 22:37:25 -0600

    finish guessing game

--- rust-begin/guessing_game/src/main.rs
+++ rust-begin/guessing_game/src/main.rs
@@ -28,26 +28,36 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
-    // The gen_range method takes a range expression as an argument and generates a random number in the range. 
-    // The kind of range expression we’re using here takes the form start..end and is inclusive on the lower bound but
-    // exclusive on the upper bound, so we need to specify 1..101 to request a number between 1 and 100. Alternatively,
-    // we could pass the range 1..=100, which is equivalent.
 
-    println!("The secret number is: {}", secret_number);
+    loop {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
+        let mut guess = String::new();
     // String::new, a function that returns a new instance of a String
     // The :: syntax in the ::new line indicates that new is an associated function of the String type.
     // An associated function is a function that’s implemented on a type, in this case String.
     // This new function creates a new, empty string. You’ll find a new function on many types,
     // because it’s a common name for a function that makes a new value of some kind.
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
-
