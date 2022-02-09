2c9e94b585977901bfe6cf0fffe7f23f5fa815cf
Matt <mtclifford12@gmail.com>
2022-02-08 20:57:04 -0600

    Constants

---
+++ rust-begin/concepts/target/debug/concepts.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/Matt/Desktop/rust/rust-begin/concepts/target/debug/concepts: /mnt/c/Users/Matt/Desktop/rust/rust-begin/concepts/src/main.rs
---
+++ rust-begin/concepts/target/debug/deps/concepts-97e042eef65613eb.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/Matt/Desktop/rust/rust-begin/concepts/target/debug/deps/concepts-97e042eef65613eb: src/main.rs
+
+/mnt/c/Users/Matt/Desktop/rust/rust-begin/concepts/target/debug/deps/concepts-97e042eef65613eb.d: src/main.rs
+
+src/main.rs:
