5204e11cfca4c8bcc44f1fd0e36bbce807bfa25b
Matt <mtclifford12@gmail.com>
2022-02-15 10:32:51 -0600

    Numeric Operations

--- rust-begin/concepts/src/main.rs
+++ rust-begin/concepts/src/main.rs
@@ -4,5 +4,22 @@ fn main() {
     x = 6;
     println!("The value of x is: {}", x);
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    let guess: u32 = "42".parse().expect("Not a number!");
+
+        // addition
+        let sum = 5 + 10;
+
+        // subtraction
+        let difference = 95.5 - 4.3;
+    
+        // multiplication
+        let product = 4 * 30;
+    
+        // division
+        let quotient = 56.7 / 32.2;
+        let floored = 2 / 3; // Results in 0
+    
+        // remainder
+        let remainder = 43 % 5;
 
 }
